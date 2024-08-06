67ade1c9f81502ced875e87448ee70d098393d0d
Pereira <108989220+ptpereiiradev@users.noreply.github.com>
2024-08-06 11:30:03 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["🌐 Bot Oficial da Ace Store","🎫 Se tiver alguma dúvida, abra ticket!","💻 WebSite: Em Desenvolvimento..."];
 
 
 let currentIndex = 0;
